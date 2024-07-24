bdf781e54b47e80d6f000442a9931b11c2c4014b
stevemuriithi <62249560+stevemuriithi@users.noreply.github.com>
2024-07-24 16:03:30 +0100

    Update athlete.php

---
+++ athlete.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoezi</title>
+    <link rel="stylesheet" href="zoezi.css">
+</head>
+<body>
+   
+   <div class="header">
+       <div class="container">
+          <div class="navbar">
+              <div class="logo">
+                  <a href="home.html"><img src="images/logo.png" width="125px"></a>
+              </div>
+              <nav>
+               <ul id="MenuItems">
+                  <li><a href="plans.html">Plan</a></li>
+                   <li><a href="athlete.php">Athlete</a></li>
+                   <li><a href="workout.php">Workouts</a></li>
+                   <li><a href="nutrition.html">Nutrition</a></li>
+                   <li><a href="classes.php">Classes</a></li>
+                   <li><a href="profile.php">Profile</a></li>
+                   <li><a href="logout.php">logout</a></li>
+                </ul>
+              </nav>  
+          </div>          
+       </div>
+       </div>
+       <br><br><br><br><br><br> <br><br><br><br><br><br>
+       <h1>Choose your sport</h1>
+       <br><br><br><br><br><br>
+    <div class="small-container">
+   <div class="row">
+    <div class="col-5">
+                   <a href="football.php"><img src="images/icons/football.png"></a>
+                   <h4>FOOTBALL</h4>
+               </div>
+               <div class="col-5">
+                   <a href="rugby.php"><img src="images/icons/rugby.png"></a>
+                   <h4>RUGBY</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/bicycle.png"></a>
+                   <h4>CYCLING</h4>
+               </div>
+           </div>
+           <br><br><br>
+           <div class="row">
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/f1.png"></a>
+                   <h4>RACING</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/bball.png"></a>
+                   <h4>BASKET BALL</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/boxing.png"></a>
+                   <h4>BOXING</h4>
+               </div>
+           </div>
+           <br><br><br>
+           <div class="row">
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/tennis.png"></a>
+                   <h4>TENNIS</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/ice.png"></a>
+                   <h4>ICE HOCKEY</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/volleyball.png"></a>
+                   <h4>VOLLEYBALL</h4>
+               </div>
+           </div>
+           <br><br><br>
+           <div class="row">
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/swimming.png"></a>
+                   <h4>SWIMMING</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/karate.png"></a>
+                   <h4>KARATE</h4>
+               </div>
+               <div class="col-5">
+                   <a href="#"><img src="images/icons/track-star.png"></a>
+                   <h4>TRACK</h4>
+               </div>
+           </div>
+
+</div>
+<br><br><br><br><br><br> <br><br><br><br><br><br>
+    
+<!--Footer--> 
+<div class="footer">
+    <div class="container">
+        <div class="row">
+            <div class="footer-col1">
+               
+                <address>
+                        <h3>ZOEZI</h3>
+                        London, United Kingdom <br>
+                        www.joka.com<br>
+                        phone: +44 123 456 7890
+                    </address>
+            </div>
+            <div class="footer-col2">
+               <h3>Download Our App</h3>
+                <p>Download App for Android and ios mobile phone.</p>
+                <div class="app-logo">
+                    <img src="images/play-store.png">
+                    <img src="images/app-store.png">
+                </div>
+            </div>
+            <div class="footer-col3">
+               <h3>Company info</h3>
+                <ul>
+                 <li><a href="#" class="footer-link">About us</a></li>
+                 <li><a href="#" class="footer-link">News</a></li>
+                 <li><a href="#" class="footer-link">Careers</a></li>
+                 <li><a href="#" class="footer-link">Stores</a></li>
+                 <li><a href="#" class="footer-link">Sustainability</a></li>
+               </ul>
+            </div>
+            <div class="footer-col4">
+               <h3>Follow Us</h3>
+                <ul>
+                    <li><a href="#" class="footer-link">LinkedIn</a></li>
+                    <li><a href="#" class="footer-link">Instagram</a></li>
+                    <li><a href="#" class="footer-link">Facebook</a></li>
+                    <li><a href="#" class="footer-link">Twitter</a></li>
+                </ul>
+            </div>
+        </div>
+        <hr>
+        <div class="order-2">
+                        <a href="#" class="footer-link">Cookie settings |</a>
+                        <a href="#" class="footer-link ml-3">Terms an conditions |</a>
+                        <a href="#" class="footer-link ml-3">privacy settings |</a>
+                    </div>
+    </div>    
+</div>
+
+</body>
+</html>
+
