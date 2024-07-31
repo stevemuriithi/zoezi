ce10df013e011a66ead9eab990e4f352a9f29e00
stevemuriithi <62249560+stevemuriithi@users.noreply.github.com>
2024-07-31 15:26:19 +0100

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,8 +1,9 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+     <?php
      include "connection.php";
      ?>
-     <!DOCTYPE html>
-<html lang="en">
+     
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
